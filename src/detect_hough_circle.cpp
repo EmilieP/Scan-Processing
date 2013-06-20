c5ab40d30ea1646204d7803bd4003e86018b6ac2
Emilie Podczaszy <epo@belighted.com>
2013-06-20 17:10:57 +0200

    If argv is a directory then read all the graphics files

--- src/detect_hough_circle.cpp
+++ src/detect_hough_circle.cpp
@@ -1,9 +1,17 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <dirent.h>
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include <sys/stat.h>
 
 using namespace cv;
+
+const std::string extensions[] = { "jpg", "png", "jpeg" };
+const int extensions_size = sizeof(extensions)/sizeof(std::string);
 
 Mat applyFilters( Mat& img)
 {
@@ -34,11 +42,11 @@ Point getCenterCoordinate( vector<Vec3f> circles )
   {
     for( size_t i = 0; i < circles.size(); i++ )
     {
-      int radius = cvRound(circles[i][2]);
+      int radius = cvRound( circles[i][2] );
 
       if( radius >= 10 && radius <= 30 ) {
-        center.x = cvRound(circles[i][0]);
-        center.y = cvRound(circles[i][1]);
+        center.x = cvRound( circles[i][0] );
+        center.y = cvRound( circles[i][1] );
         break;
       }
     }
@@ -46,52 +54,96 @@ Point getCenterCoordinate( vector<Vec3f> circles )
   return center;
 }
 
-void detectRightCircle( Mat& img )
+void detect_right_circle( Mat& img )
 {
   int width = img.cols;
   Rect roi( width - 150 , 0 , 150, 150 );
   Mat sub_img = img( roi );
-  Mat gray = applyFilters( sub_img );
+  Mat gray    = applyFilters( sub_img );
   vector<Vec3f> circles = getCircles( gray );
   Point center = getCenterCoordinate( circles );
-  center.x +=  width - 150;
+  center.x += width - 150;
 
-  printf("== Cercle de droite ==\n");
-  printf("x : %i\n", center.x );
-  printf("y : %i\n", center.y );
+  std::cout << " droite : " << "[ " << center.x << ", "<< center.y << " ]" << std::endl;
 
   circle( img, center, 1, Scalar(0,0,255), -1, 8, 0 );
 }
 
-void detectLeftCircle( Mat& img )
+void detect_left_circle( Mat img )
 {
   Rect roi( 0 , 0 , 150, 150 );
+
   Mat sub_img = img( roi );
   Mat gray = applyFilters( sub_img );
   vector<Vec3f> circles = getCircles( gray );
   Point center = getCenterCoordinate( circles );
 
-  printf("== Cercle de gauche ==\n");
-  printf("x : %i\n", center.x );
-  printf("y : %i\n", center.y );
+  std::cout << " gauche : " << "[ " << center.x << ", "<< center.y << " ]" << std::endl;
 
   circle( img, center, 1, Scalar(0,255,0), -1, 8, 0 );
 }
 
+bool is_graphic_file( std::string filename ) {
+  for( int i = 0; i < extensions_size; i++)
+  {
+    std::size_t found = filename.find(extensions[i]);
+    if (found != std::string::npos)
+      return 1;
+  }
+  return 0;
+}
+
+bool is_dir(const char* path) {
+  struct stat buf;
+  stat(path, &buf);
+  return S_ISDIR(buf.st_mode);
+}
+
+std::string full_path( std::string dir, std::string filename )
+{
+  return dir + filename;
+}
+
+void detect_circles_from_image( Mat img ){
+  detect_left_circle( img );
+  detect_right_circle( img );
+}
+
 int main(int argc, char** argv)
 {
-  Mat src = imread( argv[1], 1 );
+  DIR *dir;
+  struct dirent *file;
 
-  if( !src.data )
-    return -1;
+  if ( is_dir( argv[1] ) )
+  {
+    std::cout << argv[1] << " is a directory" << std::endl;
 
-  detectLeftCircle( src );
-  detectRightCircle( src );
+    dir = opendir( argv[1] );
 
-  namedWindow( "img");
-  moveWindow("img", 1500, 600);
-  imshow( "img", src );
-  waitKey(0);
+    if( dir != NULL ) {
+      std::string folder = argv[1];
+      while( ( file = readdir( dir ) ) )
+      {
+        std::string filename = file->d_name;
+        if( is_graphic_file( filename ) )
+        {
+          std::string img_full_path = full_path( folder, filename );
+          Mat img = imread( img_full_path, 1 ) ;
+          std::cout << " == " << filename << " == " << std::endl;
+          detect_circles_from_image( img );
+        }
+      }
+    }
+  }
+  else
+  {
+    std::cout << argv[1] << " is a file" << std::endl;
+    if( is_graphic_file( argv[1] ) )
+    {
+      Mat img = imread( argv[1], 1 );
+      detect_circles_from_image( img );
+    }
+  }
 
   return 0;
 }
